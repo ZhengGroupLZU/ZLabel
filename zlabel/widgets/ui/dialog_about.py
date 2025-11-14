5c72f6b6ef50ad66ed58d33967b1b5225c205db0
rainy liu <rainyliusy3@gmail.com>
2025-11-14 10:17:01 +0800

    update about dialog

--- zlabel/widgets/ui/dialog_about.py
+++ zlabel/widgets/ui/dialog_about.py
@@ -16,37 +16,33 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QDialog, QLabel, QSizePolicy,
-    QVBoxLayout, QWidget)
+    QTextBrowser, QVBoxLayout, QWidget)
 import icons_rc
 
 class Ui_DialogAbout(object):
     def setupUi(self, DialogAbout):
         if not DialogAbout.objectName():
             DialogAbout.setObjectName(u"DialogAbout")
-        DialogAbout.resize(550, 280)
+        DialogAbout.resize(550, 372)
         DialogAbout.setMinimumSize(QSize(550, 280))
-        DialogAbout.setMaximumSize(QSize(550, 280))
+        DialogAbout.setMaximumSize(QSize(550, 500))
         self.verticalLayout = QVBoxLayout(DialogAbout)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.label_2 = QLabel(DialogAbout)
-        self.label_2.setObjectName(u"label_2")
-        font = QFont()
-        font.setFamilies([u"Arial"])
-        font.setPointSize(71)
-        self.label_2.setFont(font)
-        self.label_2.setAlignment(Qt.AlignCenter)
+        self.textBrowser = QTextBrowser(DialogAbout)
+        self.textBrowser.setObjectName(u"textBrowser")
+        self.textBrowser.setOpenExternalLinks(True)
 
-        self.verticalLayout.addWidget(self.label_2)
+        self.verticalLayout.addWidget(self.textBrowser)
 
         self.label_4 = QLabel(DialogAbout)
         self.label_4.setObjectName(u"label_4")
         self.label_4.setMaximumSize(QSize(16777215, 40))
-        font1 = QFont()
-        font1.setPointSize(12)
-        self.label_4.setFont(font1)
-        self.label_4.setLayoutDirection(Qt.LeftToRight)
-        self.label_4.setStyleSheet(u"color: rgb(133, 0, 0);")
-        self.label_4.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+        font = QFont()
+        font.setPointSize(12)
+        self.label_4.setFont(font)
+        self.label_4.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+        self.label_4.setStyleSheet(u"color: rgb(18,115,166);")
+        self.label_4.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
 
         self.verticalLayout.addWidget(self.label_4)
 
@@ -58,7 +54,17 @@ class Ui_DialogAbout(object):
 
     def retranslateUi(self, DialogAbout):
         DialogAbout.setWindowTitle(QCoreApplication.translate("DialogAbout", u"about", None))
-        self.label_2.setText(QCoreApplication.translate("DialogAbout", u"ZLabel", None))
-        self.label_4.setText(QCoreApplication.translate("DialogAbout", u"ZLabel Copyright (C) 2023 Rainyl@ZhengGroup.", None))
+        self.textBrowser.setHtml(QCoreApplication.translate("DialogAbout", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"hr { height: 1px; border-width: 0; }\n"
+"li.unchecked::marker { content: \"\\2610\"; }\n"
+"li.checked::marker { content: \"\\2612\"; }\n"
+"</style></head><body style=\" font-family:'Microsoft YaHei UI'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><img src=\":/icon/icons/zlabel.svg\" height=\"100\" /></p>\n"
+"<p align=\"center\" dir='rtl' style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:36pt;\">ZLabel</span></p>\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-in"
+                        "dent:0; text-indent:0px;\"><span style=\" font-size:12pt;\">An image data labeling tool powered by advanced AI models.</span></p></body></html>", None))
+        self.label_4.setText(QCoreApplication.translate("DialogAbout", u"ZLabel Copyright (C) 2025 Rainyl@ZhengGroup.", None))
     # retranslateUi
 
